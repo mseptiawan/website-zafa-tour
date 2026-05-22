a014623d412a88e14490b67b5508b56006bd2df1
mseptiawan <mseptiawan017@gmail.com>
2026-05-22 19:42:49 +0700

    add: tambah mode loan

---
+++ src/models/loan/Loan.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const loanSchema = new mongoose.Schema(
+  {
+    employeeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amountRequested: {
+      type: Number,
+      required: true,
+    },
+    tenorMonths: {
+      type: Number,
+      enum: [3, 6],
+      required: true,
+    },
+    monthlyDeduction: {
+      type: Number,
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED", "CANCELLED"],
+      default: "PENDING",
+    },
+    paymentProof: {
+      type: String,
+      default: null,
+    },
+    disbursementDate: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Loan", loanSchema);
