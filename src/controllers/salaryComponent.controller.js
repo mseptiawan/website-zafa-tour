b4cb7dcc0679ab743cd6a093b389b959f56c8148
mseptiawan <mseptiawan017@gmail.com>
2026-05-24 13:15:03 +0700

    feat: tambah controller components

---
+++ src/controllers/salaryComponent.controller.js
@@ -0,0 +1,30 @@
+import * as salaryComponentService from "../services/salaryComponentService.js";
+
+export const renderManagePage = async (req, res) => {
+  try {
+    const components = await salaryComponentService.fetchAllComponents();
+    
+    res.render("payroll/components", {
+      title: "Kelola Komponen Gaji",
+      user: req.user, 
+      components: components
+    });
+  } catch (error) {
+    res.status(500).render("errors/500", { message: error.message });
+  }
+};
+
+export const getAllComponents = async (req, res) => {
+  try {
+    const components = await salaryComponentService.fetchAllComponents();
+    res.status(200).json({
+      success: true,
+      data: components
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
